ee740a44a1f3f26be149ad0f533757b554791146
Atul Malakar <atulmalakar15@gmail.com>
2020-06-05 01:36:12 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// This index files keeps a list of all the components here..
+// The file name actually cannot be changed and is fixed to index.js
+// Really saves the mess at App.js !
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
